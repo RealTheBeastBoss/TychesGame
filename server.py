f9af2509cfe05dfb905b860998461e0884cd18e9
MainLaptop <jack.cumbers@outlook.com>
2023-10-19 19:39:13 +0100

    Day Thirty Six, Part Nine: - Testing Cross Device Communication - Network Data Too Big - Now Sends 2048 * 3

--- server.py
+++ server.py
@@ -25,7 +25,7 @@ class Server:
 def threaded_client(conn, ip):
     while True:  # Send and Receive Data
         try:
-            data = pickle.loads(conn.recv(2048*2))
+            data = pickle.loads(conn.recv(2048*3))
             if not data:
                 print(ip[0] + " Disconnected")
                 break
